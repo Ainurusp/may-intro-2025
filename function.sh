acb7edb132f7077e0b33a4d9b2e13b7af26acf2f
Ainurusp <auspeyeva@gmail.com>
2025-06-07 22:04:52 +0000

    some scripts

---
+++ function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function user() {
+	sudo useradd tim
+	sudo useradd ann
+	sudo useradd brad
+
+}
+
+function file() {
+	mkdir folder1
+	touch file1
+	touch folder1/new
+}
+
+function permission () {
+	sudo chown tim folder1
+	sudo chown ann file1
+}
+
+user
+file
+permission
